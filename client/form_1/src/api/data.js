e90efd09fdaea0d2ade4cb4e5ab7f8684d16721e
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2023-04-16 17:26:17 +0530

    Update data.js

--- client/form_1/src/api/data.js
+++ client/form_1/src/api/data.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const url="https://egs-project-1-backend.onrender.com/";
+const url="https://billing-system-backend.onrender.com";
 
 export const getData =  () => axios.get(url);
